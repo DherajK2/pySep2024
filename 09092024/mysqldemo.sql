d618c3cf1306ca14aa976cc218831e2681280ae2
DherajK2 <151643194+DherajK2@users.noreply.github.com>
2024-09-09 16:19:08 +0530

    Update mysqldemo.sql

--- 09092024/mysqldemo.sql
+++ 09092024/mysqldemo.sql
@@ -54,3 +54,15 @@ WHERE id=5;
 
 DELETE FROM Person
 WHERE id=5;
+
+
+-- departments {id, name}
+CREATE TABLE departments
+(
+    id INT PRIMARY KEY auto_increment,
+    name VARCHAR(100) NOT NULL
+);
+DESC departments;
+SELECT * FROM departments;
+INSERT INTO departments(name) 
+VALUES ('Analysis'),('Design'),('Development'),('Testing');
